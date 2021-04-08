781887d63588bfc25c24d338615fb6123cfc9323
Ajitesh Bansal <abansa22@uic.edu>
2021-04-09 01:02:19 +0500

    projects file added

---
+++ projects.js
@@ -0,0 +1,12 @@
+var projects = [
+  {
+    "name": "Project 1",
+    "description": "this is a test project",
+    "techs": "HTML, CSS, JAVASCRIPT",
+    "link": "",
+    "linkPage": "",
+    "image": "",
+    "display": true
+  },
+
+]
